9571eba85e458b4cba70256a512d9e02a26667f0
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-07-12 17:09:25 +0000

    [Update] 更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -11,10 +7,19 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @books = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def destroy
   end
 
   private
