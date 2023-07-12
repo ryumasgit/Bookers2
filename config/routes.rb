ab3c00470901572b17cb6ffd176d2f70ee11c144
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-07-12 14:43:24 +0000

    [Update] 更新

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :create, :index, :show]
   resources :users, only: [:index, :show, :edit]
 
   get 'homes/about' => 'homes#about', as: 'about'
