853392c9de3b974ea4b20ca6aee6eec8892fed5a
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-07-15 05:12:09 +0000

    [Update] 更新

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -23,7 +23,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.new
     @books = Book.find(params[:id])
-    @user = current_user
+    @user = @books.user
   end
 
   def edit
