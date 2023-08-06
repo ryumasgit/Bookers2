7ee5267ec7f45a5574d08bdde9a78019c5869f51
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-08-06 14:10:29 +0000

    [Updare] 更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
 
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
+
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 
